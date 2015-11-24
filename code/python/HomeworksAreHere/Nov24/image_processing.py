d9849fb1455a977e66e1a3cbb08bfaecdc118c9c
Tommy Jun <tbj2cu@virginia.edu>
2015-11-24 15:56:31 -0500

    Made a better blackwhite converter and added an intensifier

---
+++ code/python/HomeworksAreHere/Nov24/image_processing.py
@@ -0,0 +1,79 @@
+import pygame as pg
+
+# grayPixel: pixel -> pixel
+# compute and return a gray pixel with the same intensity
+# as the given pixel
+def grayPixel(pixel):
+    red_intensity = pixel[0]
+    green_intensity = pixel[1]
+    blue_intensity = pixel[2]
+    fixRed = int(pixel[0]*0.2989)
+    fixGreen = int(pixel[1]*0.5870)
+    fixBlue = int(pixel[2]*0.1140)
+    ave_intensity = (fixRed+fixGreen+fixBlue)
+    return (ave_intensity,ave_intensity,ave_intensity)
+
+# channel: pixel -> channel -> pixel
+# return a gray pixel with intensity from given channel of given pixel
+def channel(pixel,chan):
+    return (pixel[chan],pixel[chan],pixel[chan])
+
+
+# inverse: pixel -> pixel
+# return the color negative of the given pixel
+def inverse(pixel):
+    return (255-pixel[0], 255-pixel[1], 255-pixel[2])
+
+# intensify: pixel -> nat255 -> pixel
+# brighten each channel of pixel by quantity
+def intensity(pixel,quantity):
+    pixel = (pixel[0]+quantity, pixel[1]+quantity, pixel[2]+quantity)
+    lpix = list(pixel)
+    for n in range(3):
+        if lpix[n] > 254:
+            lpix[n] = 255
+    pixel = tuple(lpix)
+    return (pixel)
+
+def bw(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = grayPixel(pixels3d[x,y])
+
+def invert(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = inverse(pixels3d[x,y])
+
+def intensify (image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = intensity(pixels3d[x,y],10)
+
