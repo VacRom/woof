aa93e59397f9447ab71ffa56f801186819d10b2e
Tommy Jun <tbj2cu@virginia.edu>
2015-11-26 09:41:49 -0500

    First commit for the new project. Added top-down movement capability. Added Actors, Walls, and Background. Added a color scheme.

---
+++ Project/colors.py
@@ -0,0 +1,39 @@
+'Base HEX value: F93D2F'
+'30 deg. Double Complementary (tetradic)'
+'www.perbang.dk/color+scheme'
+
+WHITE = (238, 255, 255)
+BLACK = (17, 17, 16)
+
+'Base Red'
+RED = (249, 61, 47)
+'Faded Pink'
+RED_0 = (255, 207, 204)
+'Pinkish Red'
+RED_1 = (255, 160, 153)
+'Dark Red'
+RED_2 = (168, 11, 0)
+'Darker Red'
+RED_3 = (136, 17, 17)
+'Reddish-Brown'
+RED_4 = (102, 0, 0)
+
+'Base Brown'
+BROWN = (242, 190, 121)
+'Light Brown'
+BROWN_0 = (153, 102, 34)
+'Very Deep Brown'
+BROWN_1 = (34, 17, 0)
+'Moderate Brown'
+BROWN_2 = (127, 100, 63)
+
+'Base Blue'
+BLUE = (17, 68, 136)
+'Faded Blue'
+BLUE_0 = (187, 204, 221)
+'Light Blue'
+BLUE_1 = (170, 187, 221)
+'Dead Blue'
+BLUE_2 = (85, 102, 119)
+'Cyan'
+BLUE_3 = (17, 119, 136)
---
+++ Project/game.py
@@ -0,0 +1,180 @@
+import pygame as pg
+import colors
+
+'Define the Actor as your character.'
+class Actor(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pg.Surface([15, 15])
+        self.image.fill(colors.RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_x = 0
+        self.vel_y = 0
+        self.walls = None
+
+    def changevel(self, x, y):
+        self.vel_x += x
+        self.vel_y += y
+
+    def update(self):
+        self.rect.x += self.vel_x
+        all_blocks = pg.sprite.spritecollide(self, self.walls, False)
+        for block in all_blocks:
+            if self.vel_x > 0:
+                self.rect.right = block.rect.left
+            else:
+                self.rect.left = block.rect.right
+        self.rect.y += self.vel_y
+        all_blocks = pg.sprite.spritecollide(self, self.walls, False)
+        for block in all_blocks:
+            if self.vel_y > 0:
+                self.rect.bottom = block.rect.top
+            else:
+                self.rect.top = block.rect.bottom
+
+'Define a Wall as an inanimate object that blocks Actors.'
+class Wall(pg.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pg.Surface([width, height])
+        self.image.fill(colors.BROWN_0)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+'Define the Background as inanimate objects that do not block Actors'
+class Background(pg.sprite.Sprite):
+    def __init__(self, x, y, width, height, color):
+        super().__init__()
+        self.image = pg.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+pg.init()
+
+screen_x = 1200
+screen_y = 800
+screen = pg.display.set_mode([screen_x, screen_y])
+pg.display.set_caption('Project')
+
+all_sprites = pg.sprite.Group()
+all_walls = pg.sprite.Group()
+all_backgrounds = pg.sprite.Group()
+
+'Generates the background.'
+floor = Background(0, 0, 800, 600, colors.BROWN)
+all_backgrounds.add(floor)
+all_sprites.add(floor)
+
+'Generates the stage.'
+wall = Wall(0, 0, 10, 600)
+all_walls.add(wall)
+all_sprites.add(wall)
+
+wall = Wall(10, 0, 790, 10)
+all_walls.add(wall)
+all_sprites.add(wall)
+
+wall = Wall(790, 10, 10, 590)
+all_walls.add(wall)
+all_sprites.add(wall)
+
+wall = Wall(10, 590, 790, 10)
+all_walls.add(wall)
+all_sprites.add(wall)
+
+'Generates actors.'
+player = Actor(50, 50)
+player.walls = all_walls
+all_sprites.add(player)
+
+clock = pg.time.Clock()
+done = False
+
+while not done:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            done = True
+
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                player.changevel(-2, 0)
+            elif event.key == pg.K_RIGHT:
+                player.changevel(2, 0)
+            elif event.key == pg.K_UP:
+                player.changevel(0, -2)
+            elif event.key == pg.K_DOWN:
+                player.changevel(0, 2)
+
+        elif event.type == pg.KEYUP:
+            if event.key == pg.K_LEFT:
+                player.changevel(2, 0)
+            elif event.key == pg.K_RIGHT:
+                player.changevel(-2, 0)
+            elif event.key == pg.K_UP:
+                player.changevel(0, 2)
+            elif event.key == pg.K_DOWN:
+                player.changevel(0, -2)
+
+    all_sprites.update()
+    screen.fill(colors.BLACK)
+    all_sprites.draw(screen)
+    pg.display.flip()
+    clock.tick(120)
+
+pg.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
